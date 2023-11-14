6718be3dfe0b7d749624ea049690223b2ad40442
claudia.quezada <claudia.quezada@tisal.cl>
2023-11-14 13:37:45 -0300

    listado-tareas spec

--- src/app/tareas/listado-tareas/listado-tareas.component.spec.ts
+++ src/app/tareas/listado-tareas/listado-tareas.component.spec.ts
@@ -23,4 +23,47 @@ describe('ListadoTareasComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('tareaDelete: Debería eliminar la primera tarea.', ()=> {
+    component.tareaDelete(0);
+    expect(service.tareas.length).toBe(3);
+  });
+
+  it('tareaCompletar: Debería completar la primera tarea.', ()=> {
+    component.tareaCompletar(0);
+    expect(service.tareas[0].completada).toBe(true);
+  });
+
+  it('get tareas: Debería traer todas las tareas del servicio.', ()=> {
+    component.tareas;
+    expect(component.tareas).toBeTruthy();
+    expect(component.tareas.length).toBe(4);
+  });
+
+  it('Debería renderizar un HTMLElement clase table', ()=> {
+    const render: HTMLElement = fixture.nativeElement;
+    const table = render.querySelector('.table');
+    console.log('table html: ',table);
+    expect(table).toBeTruthy();
+  });
+
+  it('Debería renderizar una tabla con las columnas ID, Actividad, Completar y Eliminar', ()=> {
+    const render: HTMLElement = fixture.nativeElement;
+    const table = render.querySelector('.table')?.innerHTML;
+    console.log('table html: ',table);
+    expect(table).toContain('>ID</th>');
+    expect(table).toContain('>Actividad</th>');
+    expect(table).toContain('>Completar</th>');
+    expect(table).toContain('>Eliminar</th>');
+  });
+
+  it('Debería renderizar los botones Completar y Eliminar dentro de la tabla', ()=> {
+    const render: HTMLElement = fixture.nativeElement;
+    const btnCompletar = render.querySelector('.button.is-link');
+    const btnEliminar = render.querySelector('.button.is-danger');
+    console.log('btnCompletar html: ',btnCompletar);
+    console.log('btnEliminar html: ',btnEliminar);
+    expect(btnCompletar).toBeTruthy();
+    expect(btnEliminar).toBeTruthy();
+  });
 });

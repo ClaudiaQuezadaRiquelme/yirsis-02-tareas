b1b3b44713580c5198ee119e51c18e0a9381ca2b
claudia.quezada <claudia.quezada@tisal.cl>
2023-11-14 12:11:21 -0300

    inject(TareasService)

--- src/app/tareas/listado-tareas/listado-tareas.component.spec.ts
+++ src/app/tareas/listado-tareas/listado-tareas.component.spec.ts
@@ -1,22 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ListadoTareasComponent } from './listado-tareas.component';
+import { TareasService } from '../services/tareas.service';
 
-// describe('ListadoTareasComponent', () => {
-//   let component: ListadoTareasComponent;
-//   let fixture: ComponentFixture<ListadoTareasComponent>;
+describe('ListadoTareasComponent', () => {
+  let component: ListadoTareasComponent;
+  let fixture: ComponentFixture<ListadoTareasComponent>;
+  let service: TareasService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ListadoTareasComponent],
-//       schemas: [NO_ERRORS_SCHEMA]
-//     });
-//     fixture = TestBed.createComponent(ListadoTareasComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListadoTareasComponent],
+      providers: [TareasService],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(ListadoTareasComponent);
+    component = fixture.componentInstance;
+    service = TestBed.inject(TareasService);
+    fixture.detectChanges();
+  });
 
-//   // it('should create', () => {
-//   //   expect(component).toBeTruthy();
-//   // });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/tareas/subtitulo/subtitulo.component.spec.ts
+++ src/app/tareas/subtitulo/subtitulo.component.spec.ts
@@ -1,22 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { SubtituloComponent } from './subtitulo.component';
+import { TareasService } from '../services/tareas.service';
 
-// describe('SubtituloComponent', () => {
-//   let component: SubtituloComponent;
-//   let fixture: ComponentFixture<SubtituloComponent>;
+describe('SubtituloComponent', () => {
+  let component: SubtituloComponent;
+  let fixture: ComponentFixture<SubtituloComponent>;
+  let service: TareasService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [SubtituloComponent],
-//       schemas: [NO_ERRORS_SCHEMA]
-//     });
-//     fixture = TestBed.createComponent(SubtituloComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SubtituloComponent], // instancia de ngModule,
+      providers: [TareasService],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(SubtituloComponent);
+    component = fixture.componentInstance;
+    service = TestBed.inject(TareasService);
+    fixture.detectChanges();
+  });
 
-//   // it('should create', () => {
-//   //   expect(component).toBeTruthy();
-//   // });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
